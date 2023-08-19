c1396b7b1f1cd3a2b3e3baab47ab0efe7cf13d33
jahnavi.chinni <jahnavi.chinni@192.168.60.16>
2023-08-19 10:19:06 +0530

    Webservices

--- user/src/main/java/com/spring/user/UserApplication.java
+++ user/src/main/java/com/spring/user/UserApplication.java
@@ -2,12 +2,10 @@ package com.spring.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EntityScan("com.spring.user.model")
 public class UserApplication {
 
 	public static void main(String[] args) 
